6f785f70d551e7737982fe825e6ffc796b5152ad
umairisrar <umair_israr92@hotmail.com>
2016-08-14 08:22:53 -0700

    as

--- app.js
+++ app.js
@@ -2,40 +2,6 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var app = express();
 var router = express.Router();
-app.use('/sayHello', router);
-router.get('/', handleSayHello); // handle the route at yourdomain.com/sayHello
-
-function handleSayHello(req, res) {
-    // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'umair.israr92@gmail.com', // Your email id
-            pass: '352010203576' // Your password
-        }
-    });
-
-  var text = 'Hello world from \n\n' + req.body.name;
-  var mailOptions = {
-    from: 'umair.israr92@gmail.com', // sender address
-    to: 'DP+issue@comp7000.cloudintra.com', // list of receivers
-    //to: req.body.project_key+req.body.item_type+'@comp7000.cloudintra.com', // list of receivers
-    subject: 'Test email from bot', // Subject line
-    text: text //, // plaintext body
-    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-};
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-        res.json({yo: 'error'});
-    }
-    else{
-        console.log('Message sent: ' + info.response);
-        res.json({yo: info.response});
-    }
-});
-
-}
 
 
 app.post('/email', function (req, res) {
